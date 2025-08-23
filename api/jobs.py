992f64799c29e8619590eca563bf4ae77a4adc08
cankoc35 <kocmustafacan@gmail.com>
2025-08-24 00:38:22 +0300

    " llm db functions added"

--- api/jobs.py
+++ api/jobs.py
@@ -1,14 +1,61 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 import psycopg
-from db.sql.queries import insert_embeddings_query, get_documents_query
+import re
+from db.sql.queries import *
 
-DSN = os.getenv("DSN")
+PROD_DB = os.getenv("PROD_DB")
+LOCAL_DB = os.getenv("LOCAL_DB")
 OLLAMA_HOST = os.getenv("OLLAMA_HOST")
 
-def get_conn():
-    conn = psycopg.connect(DSN)
+def get_rag_db_conn():
+    conn = psycopg.connect(LOCAL_DB)
     with conn.cursor() as cur:
-        # ensure every session knows where Ollama is
         cur.execute("SELECT set_config('ai.ollama_host', %s, false);", (OLLAMA_HOST,))
     return conn
+
+def get_prod_connection():
+    conn = psycopg.connect(PROD_DB)
+    return conn
+
+def get_documents():
+    with get_prod_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(get_documents_query)
+            documents = cur.fetchall()
+    
+    return documents
+        
+def insert_embeddings():
+    documents = get_documents()
+
+    with get_rag_db_conn() as conn:
+        with conn.cursor() as cur:
+            for doc in documents:
+                print(f"Inserting document: {doc[0]}")
+                cur.execute(insert_embeddings_query, (doc[0], doc[0]))
+        conn.commit()
+   
+def get_response():
+    user_question = '59328 numaralı sevkiyat hakkında bilgi ver'
+    
+    with get_rag_db_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute(hybrit_search_query, (user_question, user_question))
+            documents = cur.fetchone()[0]
+            
+            documents = re.sub(r'\\n+', ' ', documents)
+            documents = documents.replace("'", "")
+            documents = re.sub(r'\s+', ' ', documents).strip()
+            
+            cur.execute(get_response_from_llm_query(str(user_question), str(documents)))
+
+            response = cur.fetchone()
+            return response
+    
 
--- db/sql/queries.py
+++ db/sql/queries.py
@@ -1,19 +1,80 @@
 hybrit_search_query = """
-
+    WITH queries AS (
+        SELECT 
+            %s merged_queries
+    )
+    ,extracted_terms AS (
+                SELECT array_agg(DISTINCT term) AS terms
+                FROM (
+                    SELECT unnest(regexp_matches(merged_queries, E'\\w+', 'g')) AS term FROM queries
+        ) AS sub
+    )
+    ,term_combinations AS (
+        (SELECT 
+            r, 
+            string_agg(t.term, ' | ') AS term_combination
+        FROM extracted_terms,
+            generate_series(1, array_length(terms, 1)) AS r, 
+            LATERAL (
+                SELECT unnest(terms) AS term
+                ORDER BY random() 
+                LIMIT r
+            ) AS t
+        GROUP BY r)
+        UNION ALL 
+        (
+        SELECT 
+            r, 
+            string_agg(t.term, ' & ') AS term_combination
+        FROM extracted_terms,
+            generate_series(1, array_length(terms, 1)) AS r, 
+            LATERAL (
+                SELECT unnest(terms) AS term
+                ORDER BY random() 
+                LIMIT r
+            ) AS t
+        GROUP BY r
+        )
+    )
+    ,search_query AS (
+        SELECT 
+            string_agg('(' || term_combination || ')', ' | ') tsquery_str
+        FROM term_combinations
+    )
+    ,bm25_cte AS (
+        SELECT 
+            d."documents" document, 
+            ts_rank(tsv, to_tsquery('english', tsquery_str)) AS bm25_score
+        FROM embeddings d, search_query
+        WHERE tsquery_str IS NOT NULL AND tsv @@ to_tsquery('english', tsquery_str)  
+    )
+    ,embed_cte AS (
+        SELECT 
+            d."documents" document, 
+            1 - (d.embeddings <=> (SELECT ai.ollama_embed('mxbai-embed-large', (SELECT %s)))) AS embedding_score
+        FROM embeddings d
+    )
+    , merge_documents AS (
+        SELECT 
+            COALESCE(bm25_cte.document, embed_cte.document) AS document
+        FROM embed_cte
+        LEFT JOIN bm25_cte ON embed_cte.document = bm25_cte.document 
+        ORDER BY (COALESCE(0.6 * bm25_cte.bm25_score, 0) + 0.4 * embed_cte.embedding_score) DESC
+        LIMIT 5
+    )	
+        SELECT string_agg("document",' ') AS documents
+        FROM merge_documents;
 """
 
 insert_embeddings_query = """
     INSERT INTO embeddings ("documents","embeddings")
     VALUES (%s, ai.ollama_embed('mxbai-embed-large', %s))
-    ON CONFLICT ("documents")
-    DO UPDATE SET "embeddings" = EXCLUDED."embeddings";
 """
 
 get_documents_query = """
-    WITH cte AS (
+        WITH cte AS (
         SELECT 
             s.id "shipmentId",
-            t.status,
             origin_name.name "originName",
             destination_name.name "destinationName",
             REPLACE( (array_agg( DISTINCT routes.distance)::text),'NULL','') "distance",
@@ -21,9 +82,6 @@ get_documents_query = """
             REPLACE( (array_agg( DISTINCT "carriers"."carriers")::text),'NULL','') "carriers",
             REPLACE( (array_agg( DISTINCT "parentCarriers"."parentCarriers")::text),'NULL','') "parentCarriers"
         FROM shipments s
-        INNER JOIN transportations t ON t.id = s.id 
-            AND t."deletedAt" IS NULL 
-            AND s."deletedAt" IS NULL 
         LEFT JOIN LATERAL (
             SELECT 
                 c."plateNumber" carriers
@@ -71,15 +129,14 @@ get_documents_query = """
             WHERE sr."shipmentId" = s.id 
             AND sr."deletedAt" IS NULL 
         ) routes ON TRUE 
-        WHERE s."createdAt" BETWEEN %s AND %s
-        GROUP BY 1,2,3,4
+        WHERE s."createdAt" BETWEEN now()-interval'60days' AND now()
+        GROUP BY 1,2,3
         ORDER BY 1 DESC 
     )
         SELECT 
             format(
                 '
                     Shipment Number: %s,
-                    Status of shipment: %s,
                     Starting Place of shipment (origin of shipment): %s,
                     Ending Place of shipment (destination of shipment): %s,
                     Total distance in kilometers will be travelled: %s,
@@ -89,7 +146,6 @@ get_documents_query = """
                     The time that this information was generated: %s
                 ',
                 "shipmentId",
-                "status",
                 "originName",
                 "destinationName",
                 "distance",
@@ -100,3 +156,37 @@ get_documents_query = """
             ) "document"
         FROM cte 
 """
+
+def get_response_from_llm_query(user_question, documents):
+    query = f"""
+        WITH merged_documents AS (
+            SELECT '
+            
+                "role": "shipment assistant",  
+                "user question": "{user_question}",  
+                "related documents": "{documents}", 
+                "instructions": "
+                
+                    Please respond to the users question shortly based on the provided documents. 
+                    Do not repeat the task, role and instructions.
+                    Respond to the user question with the help of the related documents.
+                    Only use the information from the related documents, never add extra information.
+                    Never repeat the user question.
+                    Answer the user question in a complete sentence.
+                    If there are multiple documents, you can use all of them to answer the question.
+                    You have to Use example sentence starter to start the response.
+                    You have to Use example sentence ender to end the response.
+                    
+                ",
+                "example sentence starter": "I am your UstaBot helper",
+                "example sentence ender": "I hope this information helps you. If you need more information, let me know."
+                
+            '::TEXT AS "document"
+        )
+        , llama_response AS (
+            SELECT ai.ollama_generate('llama3.2:3b',"document") response 
+            FROM merged_documents
+        )
+        SELECT (response ->> 'response')::text AS response FROM llama_response;
+    """
+    return query
